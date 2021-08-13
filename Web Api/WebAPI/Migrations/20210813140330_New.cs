a687e94fc879f0cf2e655505391269beb33d8b08
TomoriFarouk <50277985+TomoriFarouk@users.noreply.github.com>
2021-08-13 16:39:40 +0100

    C:\Users\THE USER\Documents\GitHub\1stash-api\WebAPI\

---
+++ Web Api/WebAPI/Migrations/20210813140330_New.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "AspNetUsers",
+                newName: "FirstName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "JobRole",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobRole",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "AspNetUsers",
+                newName: "FullName");
+        }
+    }
+}
---
+++ Web Api/WebAPI/Models/ApplicationUserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI.Models
+{
+	public class ApplicationUserModel
+	{
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string JobRole { get; set; }
+	}
+}
